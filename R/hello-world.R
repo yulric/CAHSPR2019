a9c051af87fc144cd5633b509358457dc23110aa
yulricsequeira <yulrics@gmail.com>
2019-05-29 14:59:26 -0400

    Added comments

--- R/hello-world.R
+++ R/hello-world.R
@@ -1,10 +1,15 @@
+# The total number of dots to print to the screen
 numOfStepsToHalifax <- 20
 
+# Go through each step
 for(i in 1:numOfStepsToHalifax) {
+    # The dots that will be printed in the current step
     dotsToPrint <- ''
+    # Going from step 1 to j, add a dot to the dotsToPrint
     for(j in 1:i) {
         dotsToPrint <- paste(dotsToPrint, '.')
     }
 
+    # Print the output for the current step
     print(cat('OTTAWA', dotsToPrint, 'HALIFAX'))
 }

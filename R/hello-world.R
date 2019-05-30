de39c0a1f0480256e8a6e4069db83a10f11c433a
yulricsequeira <yulrics@gmail.com>
2019-05-29 23:31:44 -0300

    Print a plane emoji instead of a dot for the current step

--- R/hello-world.R
+++ R/hello-world.R
@@ -4,14 +4,14 @@ numOfStepsToHalifax <- 20
 # Go through each step
 for(i in 1:numOfStepsToHalifax) {
     Sys.sleep(0.5)
-    
+
     # The dots that will be printed in the current step
     dotsToPrint <- ''
     # Going from step 1 to j, add a dot to the dotsToPrint
     for(j in 1:i) {
-        dotsToPrint <- paste(dotsToPrint, '.')
+        dotsToPrint <- paste(dotsToPrint, if(j == i) '✈' else '.')
     }
 
     # Print the output for the current step
-    cat('OTTAWA', dotsToPrint, 'HALIFAX\n')
+    cat('OTTAWA', dotsToPrint, ' HALIFAX\n')
 }
